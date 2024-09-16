56f9b9403c069aa07c814ba545fc5907a19f5fe4
abc <soo990414@edu.dju.ac.kr>
2024-09-16 18:23:01 +0900

    page

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.article.ArticleService;
+import com.example.demo.user.SiteUser;
+import com.example.demo.user.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Autowired
+    private ArticleService articleService;
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void contextLoads() {
+        SiteUser user = this.userService.getUser("test");
+        for (int i = 0; i < 300; i++) {
+            this.articleService.create("title"+i, "This is content.", user);
+        }
+    }
 
 }
